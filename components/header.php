1642e53ecc5b67a92cda09375744a7b2a8c47e6d
Mark Paspirgilis <external_github@power-of-m.de>
2018-05-21 11:08:52 +0200

    init

--- components/header.php
+++ components/header.php
@@ -1,3 +1,6 @@
 <header>
-    <div class="logo">My Website</div>
+    <div class="center_wrap">
+        <a class="logo" href="index.html">Bauservice im Norden</a>
+        <?= File::i('components/navigation.php')->get_content() ?>
+    </div>
 </header>
